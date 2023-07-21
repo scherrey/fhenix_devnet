ce9feb594a8f76ff9099e3fbe361fd5428ee9a78
Benjamin Scherrey <scherrey@biggestlab.io>
2023-07-21 17:59:37 +0200

    Initial capability of deploying WrappingERC20.sol.

---
+++ tasks/taskDeployWrappingERC20.ts
@@ -0,0 +1,9 @@
+import { task } from "hardhat/config";
+import type { TaskArguments } from "hardhat/types";
+
+task("task:deployWrappingERC20").setAction(async function (taskArguments: TaskArguments, { ethers }) {
+  const signers = await ethers.getSigners();
+  const werc20Factory = await ethers.getContractFactory("WrappingERC20");
+  const werc20 = await werc20Factory.connect(signers[0]).deploy("test", "TEST");
+  console.log("WrappingERC20 deployed to: ", await werc20.getAddress());
+});
